36b9fad9712a148fe9a6fdd25b6a0108c4294b60
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-05 23:56:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getFile(file) {
   return contents;
 }
 function setFile(file, contents) {
-  write.sync(file, JSON.stringify(contents, null, 2), { overwrite: true });
+  write.sync(file, JSON.stringify(contents), { overwrite: true });
 }
 
 class DB {
@@ -31,10 +31,6 @@ class DB {
     let data = getFile(this.file);
     return data[key];
   }
-  has(key) {
-    let data = getFile(this.file);
-    return !!data[key];
-  }
   remove(key) {
     let data = getFile(this.file);
     delete data[key];
@@ -50,73 +46,11 @@ class DB {
     })
     return match;
   }
-  keys() {
-    return Object.keys(getFile(this.file))
-  }
   all() {
     return getFile(this.file)
   }
   backup(file) {
     setFile(file, getFile(this.file));
-  }
-  add(key, number) {
-    let data = getFile(this.file);
-    data[key] = (Number(data[key]) || 0) + number;
-    setFile(this.file, data);
-  }
-  sub(key, number) {
-    let data = getFile(this.file);
-    data[key] = (Number(data[key]) || 0) - number;
-    setFile(this.file, data);
-  }
-  push(key, value) {
-    let data = getFile(this.file);
-    data[key].push(value)
-    setFile(this.file, data);
-  }
-  pull(key, index) {
-    let data = getFile(this.file);
-    data[key] = data[key].slice(0,index).concat(data[key].slice(index+1,data[key].length))
-    setFile(this.file, data);
-  }
-  pop(key) {
-    let data = getFile(this.file);
-    let value = data[key].pop()
-    setFile(this.file, data);
-    return value;
-  }
-  flat(key) {
-    let data = getFile(this.file);
-    data[key] = data[key].flat()
-    setFile(this.file, data);
-  }
-  concat(key, value) {
-    let data = getFile(this.file);
-    data[key] = data[key].concat(value)
-    setFile(this.file, data);
-  }
-  oset(key, value) {
-    // not secure, not recomended for getting stuff via user input :p
-    let data = getFile(this.file);
-    let ke = 'data["'+key.replaceAll(/[\\\/"]/g, function(match){return '\\'+match}).split('.').join('"]["')+'"] = value;';
-    eval(ke)
-    setFile(this.file, data);
-  }
-  oget(key) {
-    let data = getFile(this.file);
-    let ke = data;
-    key.split('.').forEach(ey => {
-      ke = ke[ey]
-    })
-    return ke;
-  }
-  okey(key) {
-    let data = getFile(this.file);
-    let ke = data;
-    key.split('.').forEach(ey => {
-      ke = ke[ey]
-    })
-    return Object.keys(ke);
   }
 }
 
