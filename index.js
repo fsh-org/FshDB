f01b2f43e65e9211b0ab584de4db0fc9c2a06b29
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-12-09 20:25:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 const fs = require('fs')
 
-function getFile(file) {
-  let contents = fs.readFileSync(file, 'utf8');
-  try {
-    contents = JSON.parse(contents);
-  } catch (err) {
-    throw new Error(file + ' is corrupted');
-  }
-  return contents;
-}
-function setFile(file, contents, sync = true, that = '') {
-  if (sync) {
-    fs.writeFileSync(file, JSON.stringify(contents, null, 2), { overwrite: true });
-  } else {
-    that.stream.write(JSON.stringify(contents, null, 2), err=>err);
-  }
-}
-
-function setnest(data, key, value) {
-  let p = key.split('.');
-  let target = data;
-  for (let i = 0; i < p.length - 1; i++) {
-    if (target[p[i]] === undefined) {
-      target[p[i]] = {};
-    }
-    target = target[p[i]];
-  }
-  target[p[p.length - 1]] = value;
-  return data;
-}
-
 class DB {
-  constructor(file) {
-    this.file = (file === undefined ? 'DB.json' : file);
+  constructor(file='DB.json', options={}) {
+    this.file = file ?? 'DB.json';
     if (!fs.existsSync(this.file)) {
-      setFile(this.file, {});
+      this._setFile({});
     }
+    this.compact = Boolean(options?.compact);
+  }
+  _getFile() {
+    let contents = fs.readFileSync(this.file, 'utf8');
+    try {
+      contents = JSON.parse(contents);
+    } catch (err) {
+      throw new Error(this.file + ' is corrupted');
+    }
+    return contents;
+  }
+  _setFile(contents, file=this.file) {
+    fs.writeFileSync(file,
+      (this.compact ?
+      JSON.stringify(contents) :
+      JSON.stringify(contents, null, 2)),
+      { overwrite: true });
+  }
+  _setnest(data, key, value) {
+    let p = key.split('.');
+    let target = data;
+    for (let i = 0; i < p.length - 1; i++) {
+      if (target[p[i]] === undefined) {
+        target[p[i]] = {};
+      }
+      target = target[p[i]];
+    }
+    target[p[p.length - 1]] = value;
+    return data;
   }
   set(key, value) {
-    let data = getFile(this.file);
-    data = setnest(data, key, value)
-    setFile(this.file, data);
+    let data = this._getFile();
+    data = this._setnest(data, key, value)
+    this._setFile(data);
   }
   get(key) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     let p = key.split('.');
     for (let i= 0; i < p.length; i++) {
       data = data[p[i]]
@@ -51,202 +50,72 @@ class DB {
     return data
   }
   has(key) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     return data[key] !== undefined;
   }
   remove(key) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     delete data[key];
-    setFile(this.file, data);
+    this._setFile(data);
   }
   find(func) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     let match = [];
     Object.keys(data).forEach(key => {
-      if (func(data[key])) {
+      if (func(data[key], key)) {
         match.push(key)
       }
     })
     return match;
   }
   keys() {
-    return Object.keys(getFile(this.file))
+    return Object.keys(this._getFile())
   }
   values() {
-    return Object.values(getFile(this.file))
+    return Object.values(this._getFile())
   }
   all() {
-    return getFile(this.file)
+    return this._getFile()
   }
   backup(file) {
-    setFile(file, getFile(this.file));
+    this._setFile(this._getFile(), file);
   }
   add(key, number) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     data[key] = (Number(data[key]) || 0) + number;
-    setFile(this.file, data);
+    this._setFile(data);
   }
   sub(key, number) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     data[key] = (Number(data[key]) || 0) - number;
-    setFile(this.file, data);
+    this._setFile(data);
   }
   push(key, value) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     data[key].push(value)
-    setFile(this.file, data);
+    this._setFile(data);
   }
   pull(key, index) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     data[key] = data[key].slice(0,index).concat(data[key].slice(index+1,data[key].length))
-    setFile(this.file, data);
+    this._setFile(data);
   }
   pop(key) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     let value = data[key].pop()
-    setFile(this.file, data);
+    this._setFile(data);
     return value;
   }
   flat(key) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     data[key] = data[key].flat()
-    setFile(this.file, data);
+    this._setFile(data);
   }
   concat(key, value) {
-    let data = getFile(this.file);
+    let data = this._getFile();
     data[key] = data[key].concat(value)
-    setFile(this.file, data);
-  }
-  okey(key) {
-    let data = getFile(this.file);
-    let ke = data;
-    key.split('.').forEach(ey => {
-      ke = ke[ey]
-    })
-    return Object.keys(ke);
+    this._setFile(data);
   }
 }
 
-class miniDB {
-  constructor(file) {
-    this.file = (file === undefined ? 'DB.json' : file);
-    if (!fs.existsSync(this.file)) {
-      setFile(this.file, {});
-    }
-  }
-  set(key, value) {
-    let data = getFile(this.file);
-    data[key] = value;
-    setFile(this.file, data);
-  }
-  get(key) {
-    let data = getFile(this.file);
-    return data[key];
-  }
-  has(key) {
-    let data = getFile(this.file);
-    return data[key] !== undefined;
-  }
-  remove(key) {
-    let data = getFile(this.file);
-    delete data[key];
-    setFile(this.file, data);
-  }
-  all() {
-    return getFile(this.file)
-  }
-  backup(file) {
-    setFile(file, getFile(this.file));
-  }
-}
-
-class fastDB {
-  constructor(file) {
-    this.file = (file === undefined ? 'DB.json' : file);
-    this.stream = fs.createWriteStream(file, { flags: 'w' });
-    this.cont = {};
-    if (!fs.existsSync(this.file)) {
-      setFile(this.file, {});
-    } else {
-      this.cont = getFile(this.file);
-    }
-  }
-  set(key, value) {
-    this.cont = setnest(this.cont, key, value);
-    setFile(this.file, this.cont, false, this);
-  }
-  get(key) {
-    let data = this.cont;
-    let p = key.split('.');
-    for (let i= 0; i < p.length; i++) {
-      data = data[p[i]]
-    }
-    return data
-  }
-  has(key) {
-    return this.cont[key] !== undefined;
-  }
-  remove(key) {
-    delete this.cont[key];
-    setFile(this.file, this.cont, false, this);
-  }
-  find(func) {
-    let match = [];
-    Object.keys(this.cont).forEach(key => {
-      if (func(this.cont[key])) {
-        match.push(key)
-      }
-    })
-    return match;
-  }
-  keys() {
-    return Object.keys(this.cont)
-  }
-  values() {
-    return Object.values(this.cont)
-  }
-  all() {
-    return this.cont
-  }
-  backup(file) {
-    setFile(file, this.cont);
-  }
-  add(key, number) {
-    this.cont[key] = (Number(this.cont[key]) || 0) + number;
-    setFile(this.file, this.cont, false, this);
-  }
-  sub(key, number) {
-    this.cont[key] = (Number(this.cont[key]) || 0) - number;
-    setFile(this.file, this.cont, false, this);
-  }
-  push(key, value) {
-    this.cont[key].push(value)
-    setFile(this.file, this.cont, false, this);
-  }
-  pull(key, index) {
-    this.cont[key] = this.cont[key].slice(0,index).concat(this.cont[key].slice(index+1,this.cont[key].length))
-    setFile(this.file, this.cont, false, this);
-  }
-  pop(key) {
-    let value = this.cont[key].pop()
-    setFile(this.file, this.cont, false, this);
-    return value;
-  }
-  flat(key) {
-    this.cont[key] = this.cont[key].flat()
-    setFile(this.file, this.cont, false, this);
-  }
-  concat(key, value) {
-    this.cont[key] = this.cont[key].concat(value)
-    setFile(this.file, this.cont, false, this);
-  }
-  okey(key) {
-    let ke = this.cont;
-    key.split('.').forEach(ey => {
-      ke = ke[ey]
-    })
-    return Object.keys(ke);
-  }
-}
-
-module.exports = { DB, miniDB, fastDB };
+module.exports = DB;
